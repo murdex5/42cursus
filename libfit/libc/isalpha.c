9107b44fbc2919599fd80feef992ab1c4d034f19
Kadupitige Praboth Malsha Fernando <kadferna@e3r11p8.42roma.it>
2024-12-09 16:17:24 +0100

    ft_isalpha done

--- libfit/libc/isalpha.c
+++ libfit/libc/isalpha.c
@@ -6,10 +6,18 @@
 /*   By: kadferna <kadferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 14:49:30 by kadferna          #+#    #+#             */
-/*   Updated: 2024/12/09 15:55:42 by kadferna         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:16:55 by kadferna         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
+int ft_isalpha(char c)
+{
+    if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')))
+        return (1);
+    return (0);
+}
+
+/*
 #include <stdio.h>
 int main(int ac, char **av)
 {
@@ -17,10 +25,10 @@ int main(int ac, char **av)
         return 1;
     else
     {
-        if (ft_isalpha(av[1]) == 0)
+        if (ft_isalpha(av[1][0]) == 0)
             printf("is alpha\n");
         else
             printf("not alpha\n");
     }
     return 0;
-}
+}*/

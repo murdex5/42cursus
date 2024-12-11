89f1f75d51c9d17c81a329f426b568520ab5be40
Kadupitige Praboth Malsha Fernando <kadferna@e3r11p8.42roma.it>
2024-12-11 15:39:24 +0100

    added ft_strdup.c

--- libfit/libc/ft_strdup.c
+++ libfit/libc/ft_strdup.c
@@ -10,4 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strdup(const char *s)
+{
+	char	*temp;
+	size_t	i;
 
+	if (!s)
+		return (NULL);
+	temp = malloc(sizeof(char *) * ft_strlen(s) + 1);
+	if (!temp)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		temp[i] = s[i];
+		i++;
+	}
+	temp[i] = '\0';
+	return (temp);
+}
+/*
+int	main(void)
+{
+	char *str = "Hello";
+	char *cop = ft_strdup(str);
+	printf("%s \n", cop);
+	return (0);
+}*/

3542fdd881ba778c92bf6dea7014032c4f9cfef1
murdex5 <malshapraboth@Malshas-Laptop>
2024-12-12 17:11:01 +0100

    strtrim is done

--- libfit/ft_strtrim.c
+++ libfit/ft_strtrim.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
@@ -6,45 +6,71 @@
 /*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 17:41:55 by kadferna          #+#    #+#             */
-/*   Updated: 2024/12/11 17:41:58 by kadferna         ###   ########.fr       */
+/*   Updated: 2024/12/12 14:44:23 by kadferna         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <malloc.h>
-
-static int isin(char c, char const *set)
+static int	isin(char c, char const *set)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (set[i] != '\0')
-    {
-        if (set[i] == c)
-        {
-            return (1);
-        }
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (set[i] != '\0')
+	{
+		if (set[i] == c)
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+static void check_len(char const *s1, char const *set, size_t len)
+{
+	size_t i;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (isin(s1[i], set))
+		{
+			len--;
+		}
+		i++;
+	}
 }
 
-char    *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    char    *result;
-    size_t trim_len;
-    size_t i;
+	char	*result;
+	size_t	trim_len;
+	size_t	i;
+	size_t	j;
 
-    i = 0;
-    trim_len = ft_strlen(s1);
-    while (s1[i] != '\0')
-    {
-        if (isin(s1[i], set))
-        {
-            trim_len--;
-        }
-        i++;
-    }
-    result = malloc(sizeof(char) * trim_len + 1)
+	j = 0;
+	trim_len = ft_strlen(s1);
+	check_len(s1, set, trim_len);
+	result = malloc(sizeof(char) * trim_len + 1);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (!isin(s1[i], set))
+		{
+			result[j] = s1[i];
+			j++;
+		}
+		i++;
+	}
+	return (result);
 }
+/*
+int	main(void)
+{
+	char *str = "Hello, World!";
+	char *charset = ", !l";
+	char *modstr = ft_strtrim(str, charset);
+	printf("%s\n", modstr);
+	return (0);
+}*/

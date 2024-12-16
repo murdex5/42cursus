94d03379d3579083f754fea3f8b2d4ee127da75c
Kadupitige Praboth Malsha Fernando <kadferna@e3r11p8.42roma.it>
2024-12-16 16:39:37 +0100

    phew

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -17,7 +17,10 @@ static int	get_digit(int nb)
 
 	digits = 0;
 	if (nb <= 0)
+	{
 		++digits;
+		nb = -nb;
+	}
 	while (nb != 0)
 	{
 		digits++;
@@ -26,32 +29,47 @@ static int	get_digit(int nb)
 	return (digits);
 }
 
+static char *get_mem(int len)
+{
+	char *str;
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	return (str);
+}
+
 char	*ft_itoa(int nb)
 {
 	char	*result;
 	int		len;
-
+	
+	if (nb == (int)-2147483648)
+		return ("-2147483648");
 	len = get_digit(nb);
-	result = malloc(sizeof(char) * (len + 1));
+	result = get_mem(len);
 	if (!result)
 		return (NULL);
-	result[len] = '\0';
 	if (nb == 0)
 		result[0] = '0';
 	if (nb < 0)
+	{
 		result[0] = '-';
+		nb = -nb;
+	}
 	while (nb != 0)
 	{
 		--len;
-		result[len] = (nb % 10) + '0';
+		result[len] = (nb % 10) + 48;
 		nb = nb / 10;
 	}
 	return (result);
 }
 /*
+#include <stdio.h>
 int	main(void)
 {
-	char *str = ft_itoa(0);
+	char *str = ft_itoa(-2147483648);
 	printf("%s\n", str);
 	return (0);
 }*/
--- libft/libft.h
+++ libft/libft.h
@@ -13,8 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <malloc.h>
-# include <stddef.h>
+# include <limits.h>
 # include <stdlib.h>
 # include <unistd.h>
 

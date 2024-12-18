aa249848dee683483a0c6370a6dce29de8738beb
Kadupitige Praboth Malsha Fernando <kadferna@e3r11p8.42roma.it>
2024-12-18 11:19:59 +0100

    substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -14,21 +14,26 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	sub_len;
-	char	*str;
+	size_t	str_len;
 	size_t	i;
 	char	*sub;
 
-	str = (char *)s;
 	if (!s)
 		return (NULL);
-	sub_len = ft_strlen(&s[start]);
-	sub = malloc(sizeof(char) * sub_len + 1);
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		return (NULL);
+	sub_len = str_len - start;
+	if (sub_len > len)
+		sub_len = len;
+	sub = (char *)malloc(sizeof(char) * (sub_len + 1));
 	if (!sub)
 		return (NULL);
 	i = 0;
-	while (str[start] != '\0' && i < len)
+	while (i < sub_len)
 	{
-		sub[i++] = str[start++];
+		sub[i] = s[start + i];
+		i++;
 	}
 	sub[i] = '\0';
 	return (sub);
@@ -36,8 +41,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 /*
 int	main(void)
 {
-	char    *main = "Hello, World!";
-	char    *test = ft_substr(main, 7, ft_strlen(main));
+	char	*main;
+	char	*test;
+
+	main = "Hello, World!";
+	test = ft_substr(main, 7, ft_strlen(main));
 	printf("%s\n", test);
 	free(test);
 	return (0);
--- libft/test.c
+++ libft/test.c
@@ -1,15 +1,11 @@
 #include "libft.h"
 #include <stdio.h>
 
+
 int	main(void)
 {
-	char src[] = "coucou";
-	char dest[10];
-	ft_memset(dest, 'A', 10);
-	size_t i = ft_strlcpy(dest, src, 1);
-	printf("Buffer contents: ");
-    for (int j = 0; j < 10; j++)
-        printf("%c ", dest[j]);
-	printf("\nDestination: '%s', Returned length: %zu\n", dest, i);
+	char *s = ft_substr("tripouille", 1, 1);
+	printf("%s \n", s);
+	free(s);
 	return (0);
 }

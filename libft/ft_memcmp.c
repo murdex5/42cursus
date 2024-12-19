938d9e9fca0806c340e80a1e474afb7d1668336a
murdex5 <malshapraboth@Malshas-Laptop>
2024-12-19 14:06:21 +0100

    fixed memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -17,12 +17,16 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*str1;
 	unsigned char	*str2;
 
-	i = 0;
+	if (!s1 && !s2)
+		*((char *)0) = *((char *)0);
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	while (str1[i] == str2[i] && i < n)
+	i = 0;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
-	if (i == n)
-		return (0);
-	return (str1[i] - str2[i]);
+	}
+	return (0);
 }
--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -11,20 +11,14 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 	char	*d;
 	char	*s;
 
-	if (n == 0)
-		return (dest);
-	if (!dest || !src)
-	{
-		*((char *)0) = *((char *)0);
+	if (!dest && !src)
 		return (NULL);
-	}
 	i = 0;
 	d = (char *)dest;
 	s = (char *)src;

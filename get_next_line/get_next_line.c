c9a52fa5c6c406f137fcdce93dc43c46f411abbf
Kadupitige Praboth Malsha Fernando <kadferna@e4r6p3.42roma.it>
2025-01-10 14:21:06 +0100

    no idea

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,4 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char	*ft_free(char *buffer, char *buff)
+{
+	char	*temp;
+
+	temp = ft_strjoin(buffer, buff);
+	free(buffer);
+	return (temp);
+}
+
+char	*get_line(int fd, char *res)
+{
+	char	*buffer;
+	int		read_err;
+
+	if (!res)
+		res = ft_calloc(1, 1);
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	read_err = 1;
+	while (read_err > 0)
+	{
+		read_err = read(fd, buffer, BUFFER_SIZE);
+		if (read_err == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[read_err] = 0;
+		res = ft_free(res, buffer);
+	}
+	free(buffer);
+	return (res);
+}
+char *read_line(char *)
+
+char	*get_next_line(int fd)
+{
+	static char *buffer;
+	char *line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+
+	return (line);
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <limits.h>
+
+char *get_next_line(int fd);
+char *ft_strjoin(char const *s1, char const *s2);
+void *ft_calloc(size_t nmemb, size_t size);
+size_t ft_strlen(char *str)
+#endif

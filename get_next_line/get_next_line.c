710b7bdc64a7db0429b5be90b705d1c6601c38a2
Kadupitige Praboth Malsha Fernando <kadferna@e4r6p3.42roma.it>
2025-01-10 15:26:14 +0100

    everythin is broken :(((

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,54 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/10 11:11:17 by kadferna          #+#    #+#             */
-/*   Updated: 2025/01/10 11:11:18 by kadferna         ###   ########.fr       */
+/*   Created: 2025/01/10 14:58:12 by kadferna          #+#    #+#             */
+/*   Updated: 2025/01/10 14:58:15 by kadferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_free(char *buffer, char *buff)
+char *get_next_line(int fd)
 {
-	char	*temp;
-
-	temp = ft_strjoin(buffer, buff);
-	free(buffer);
-	return (temp);
+    static char *buffer;
+    char *line;
+    return (line);
 }
 
-char	*get_line(int fd, char *res)
+char *read_line(int fd, char *res)
 {
-	char	*buffer;
-	int		read_err;
+    char *buffer;
+    int read_err;
 
-	if (!res)
-		res = ft_calloc(1, 1);
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	read_err = 1;
-	while (read_err > 0)
-	{
-		read_err = read(fd, buffer, BUFFER_SIZE);
-		if (read_err == -1)
-		{
-			free(buffer);
-			return (NULL);
-		}
-		buffer[read_err] = 0;
-		res = ft_free(res, buffer);
-	}
-	free(buffer);
-	return (res);
-}
-char *read_line(char *)
-
-char	*get_next_line(int fd)
-{
-	static char *buffer;
-	char *line;
-
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-		return (NULL);
-
-	return (line);
+    if (!res)
+        res = ft_calloc(1, 1);
+    buffer = ft_calloc(BUFFER_SIZE, sizeof(char));
+    read_err = 1;
+    while (read_err > 0)
+    {
+        read_err = read(fd, buffer, BUFFER_SIZE);
+        if (read_err == -1)
+        {
+            free(buffer);
+            return (NULL);
+        }
+        res = ft_strjoin(buffer, res);
+        free(buffer);
+    }
+    free(buffer);
+    return (res);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -11,15 +11,15 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
 # include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-char *get_next_line(int fd);
-char *ft_strjoin(char const *s1, char const *s2);
-void *ft_calloc(size_t nmemb, size_t size);
-size_t ft_strlen(char *str)
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlen(char *str);
 #endif

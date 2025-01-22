8994a3f667810eb005ca2424cc35c5d34669ea25
malshapraboth <kadferna@student.42roma.it>
2025-01-22 12:13:59 +0100

    allso did bonus cause it was easyyy

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -10,4 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BINUS_H
 
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 50
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+
+#endif

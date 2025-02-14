0ca426fa8025e009e4839f77051b81dde2d5a0e8
malshapraboth <kadferna@student.42roma.it>
2025-02-14 10:28:56 +0100

    getting the hang of it

---
+++ so_long/so_long.h
@@ -0,0 +1,12 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "my_headers/ft_priintf.h"
+# include "my_headers/libft.h"
+# include "minlibx-linux/mlx.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif

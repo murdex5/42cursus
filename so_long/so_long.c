a02007bca14b136285592d4c0b1106efeed99bb6
malsha_Vbx <malshapraboth@gmail.com>
2025-02-20 10:13:50 +0100

    squares

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -34,13 +34,29 @@ int main(void)
     void *mlx;
     void *mlx_win;
 
+    int i;
+    int j;
+
     t_data img;
 
+
+    i = 0;
+    j = 0;
     mlx = mlx_init();
     mlx_win = mlx_new_window(mlx, 1000, 1000, "Hello, world!");
     img.img = mlx_new_image(mlx, 1000, 1000);
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-    my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
+    while (i < 10)
+    {
+        j = 0;
+        while (j < 10)
+        {
+            my_mlx_pixel_put(&img, 5+i, 5+j, 0x00FF0000);
+            j++;
+        }
+        i++;
+    }    
+        
     mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
     mlx_loop(mlx);
 }

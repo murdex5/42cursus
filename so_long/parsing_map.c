1887d3388943445ee4e93f14810d0747509cba00
malshapraboth <kadferna@student.42roma.it>
2025-02-21 09:38:47 +0100

    reading the file

--- so_long/parsing_map.c
+++ so_long/parsing_map.c
@@ -12,3 +12,36 @@
 
 #include "so_long.h"
 
+int	open_file(char *file)
+{
+	int	fd;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_printf("Couldn't open the file\n");
+		perror("So long");
+		exit(1);
+	}
+	return (fd);
+}
+
+int	read_file(char *file, unsigned char *buffer)
+{
+	int	fd;
+	int	read_bytes;
+
+	fd = open_file(file);
+	if (fd == -1)
+		return (-1);
+	read_bytes = read(fd, buffer, sizeof(buffer));
+	if (read_bytes == -1)
+	{
+		close(fd);
+		ft_printf("Couldn't read the file\n");
+		perror("So long");
+		exit(1);
+	}
+	close(fd);
+	return (read_bytes);
+}

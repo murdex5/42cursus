a2048f70e22a5daa53301ee8c0aff1487f8d9484
malsha_Vbx <malshapraboth@gmail.com>
2025-02-26 12:47:15 +0100

    lessgggo

--- so_long/main.c
+++ so_long/main.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/26 10:06:08 by kadferna          #+#    #+#             */
-/*   Updated: 2025/02/26 10:06:11 by kadferna         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "so_long.h"
 
@@ -18,7 +7,7 @@ int	main(int argc, char **argv)
 	t_data data;
 
 	if (!check_file_type(argv[1], ".ber"))
-        return (error_message("Wrong File Type."));
+		return (error_message("Wrong File Type."));
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
 		return (1);

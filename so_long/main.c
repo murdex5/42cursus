60dac60c354f10df91a509d2cd4277c766a27bfc
malshapraboth <malshapraboth@gmail.com>
2025-02-28 19:04:45 +0100

    something

--- so_long/main.c
+++ so_long/main.c
@@ -4,20 +4,27 @@
 
 int	main(int argc, char **argv)
 {
-	t_data *data;
+	t_data data;
+	t_map *map;
+	int i;
 
 	if (argc != 2)
 		return (1);
-	data->map = parsing_map(argv[1]);
-	data->mlx_ptr = mlx_init();
-	data->win_ptr = mlx_new_window(data->mlx_ptr, 600, 400, "hi :)");
-	if (!data->win_ptr)
-		return (free(data->mlx_ptr), 1);
-	mlx_hook(data->win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
-	mlx_hook(data->win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
-	mlx_loop(data->mlx_ptr);
-	ft_printf("%s\n", data->map->content[1]);
-	free_mlx_pointer(data);
+	map = parsing_map(argv[1]);
+	i = 0;
+	while (i < map->height)
+	{
+		ft_printf("%s\n", map->content[i]);
+		i++;
+	}
+	data.mlx_ptr = mlx_init();
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 400, "hi :)");
+	if (!data.win_ptr)
+		return (free(data.mlx_ptr), 1);
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
+	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
+	mlx_loop(data.mlx_ptr);
+	free_map(map);
 	return (0);
 }
 

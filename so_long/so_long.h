63a9705536c771fa00ba6060afddfbcce348b7fc
malshapraboth <malshapraboth@gmail.com>
2025-03-06 16:13:46 +0100

    t_animation

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -15,43 +15,55 @@
 
 # include "libft/libft.h"
 # include "mlx/mlx.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <X11/X.h>
-# include <X11/keysym.h>
 
 /* Structs */
 
+typedef struct s_animation
+{
+	void				*img;
+	struct s_animation	*next;
+}						t_animation;
 typedef struct s_map
 {
-	char	**content;
-	int		width;
-	int		height;
-	int		player_count;
-	int		exit_count;
-	int		collectables;
-	int		player_x;
-	int		player_y;
-}			t_map;
+	char				**content;
+	int					width;
+	int					height;
+	int					player_count;
+	int					exit_count;
+	int					collectables;
+	int					player_x;
+	int					player_y;
+}						t_map;
 
+typedef struct s_player
+{
+	t_animation			*idle;
+	t_animation			*run;
+	int					h;
+	int					w;
+}						t_player;
 typedef struct s_vars
 {
-	void	*mlx;
-	void	*win;
-	//t_map	*map;
-}			t_vars;
+	void				*mlx;
+	void				*win;
+	// t_map	*map;
+}						t_vars;
 
-int			check_file_type(char *file, char *type);
-int			check_path(t_map *map);
-int			error_message(char *msg);
-int			if_map_rectangled(t_map *map);
-int			if_map_sorrounded(t_map *map);
-int			check_map_contents(t_map *map);
-int			check_map(t_map *map);
-void		free_map(t_map *map);
-void		free_map_content(char **content, int height);
-void		clean_up(t_map *map);
-t_map		*parsing_map(char *file);
+int						check_file_type(char *file, char *type);
+int						check_path(t_map *map);
+int						error_message(char *msg);
+int						if_map_rectangled(t_map *map);
+int						if_map_sorrounded(t_map *map);
+int						check_map_contents(t_map *map);
+int						check_map(t_map *map);
+void					free_map(t_map *map);
+void					free_map_content(char **content, int height);
+void					clean_up(t_map *map);
+t_map					*parsing_map(char *file);
 #endif

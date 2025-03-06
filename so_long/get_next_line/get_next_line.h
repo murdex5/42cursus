250c2f3430a83d3c12ed23af12ff1becc4c95ceb
malshapraboth <malshapraboth@gmail.com>
2025-03-06 12:49:33 +0100

    new libft

--- so_long/get_next_line/get_next_line.h
+++ so_long/get_next_line/get_next_line.h
@@ -1,13 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   player.c                                           :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/06 11:48:09 by kadferna          #+#    #+#             */
-/*   Updated: 2025/03/06 11:48:14 by kadferna         ###   ########.fr       */
+/*   Created: 2025/03/06 12:27:23 by kadferna          #+#    #+#             */
+/*   Updated: 2025/03/06 12:28:40 by kadferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "../libft/libft.h"
+
+char	*get_next_line(int fd);
+char	*get_next_line_bonus(int fd);
+
+#endif
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -13,6 +13,10 @@
 #ifndef SO_LONG_H
 #define SO_LONG_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 
 #endif

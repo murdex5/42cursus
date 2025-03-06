c50c8ab564b4954ac4c4067ffba2988355c248e5
malshapraboth <malshapraboth@gmail.com>
2025-03-06 15:05:31 +0100

    map checks

--- so_long/frees.c
+++ so_long/frees.c
@@ -11,3 +11,48 @@
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+
+void	free_map(t_map *map)
+{
+	int		i;
+	char	**map_arr;
+
+	if (!map)
+		return ;
+	map_arr = map->content;
+	i = 0;
+	while (i < map->height)
+	{
+		if (map_arr && map_arr[i])
+		{
+			free(map_arr[i]);
+			map_arr[i] = NULL;
+		}
+		i++;
+	}
+	if (map_arr)
+		free(map_arr);
+	map->content = NULL;
+	free(map);
+	map = NULL;
+}
+
+void	free_map_content(char **content, int height)
+{
+	int	i;
+
+	if (!content)
+		return ;
+	i = 0;
+	while (i < height)
+	{
+		if (content[i])
+		{
+			free(content[i]);
+			content[i] = NULL;
+		}
+		i++;
+	}
+	free(content);
+}
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -14,7 +14,7 @@
 # define SO_LONG_H
 
 # include "libft/libft.h"
-#include "mlx/mlx.h"
+# include "mlx/mlx.h"
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -24,25 +24,25 @@
 
 typedef struct s_map
 {
-	char **content;
-	int width;
-	int height;
-	int player_count;
-	int exit_count;
-	int collectables;
-	int player_x;
-	int player_y;
-} t_map;
+	char	**content;
+	int		width;
+	int		height;
+	int		player_count;
+	int		exit_count;
+	int		collectables;
+	int		player_x;
+	int		player_y;
+}			t_map;
 
-
-
-int	check_file_type(char *file, char *type);
-int	check_path(t_map *map);
-int	error_message(char *msg);
-int	if_map_rectangled(t_map *map);
-int	if_map_sorrounded(t_map *map);
-int	check_map_contents(t_map *map);
-int	check_map(t_map *map);
-void	clean_up(t_map *map);
-t_map	*parsing_map(char *file);
+int			check_file_type(char *file, char *type);
+int			check_path(t_map *map);
+int			error_message(char *msg);
+int			if_map_rectangled(t_map *map);
+int			if_map_sorrounded(t_map *map);
+int			check_map_contents(t_map *map);
+int			check_map(t_map *map);
+void		free_map(t_map *map);
+void		free_map_content(char **content, int height);
+void		clean_up(t_map *map);
+t_map		*parsing_map(char *file);
 #endif

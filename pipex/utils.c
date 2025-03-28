839c7a0dc477249f294fab91c75e33e1fba6b8d1
malsha_Vbx <malshapraboth@gmail.com>
2025-03-28 14:27:07 +0100

    write

--- pipex/utils.c
+++ pipex/utils.c
@@ -50,3 +50,29 @@ void	ft_close_fd(int in_fd, int out_fd)
 		close(out_fd);
 	}
 }
+
+void ft_write_fd(char *name, int fd_in, int hd)
+{
+	int fd_out;
+	int b_read;
+	char buff[1];
+
+	if (hd)
+		fd_out = open(name, O_RDWR | O_CREAT | O_APPEND, 00777);
+	else
+	{
+		if (access(name, F_OK) == 0)
+			unlink(name);
+		fd_out = open(name, O_WRONLY | O_CREAT, 00777);
+	}
+	if (fd_in < 0)
+		err_p("ft_write fd_out error.");
+	if (fd_in < 0)
+		err_p("ft_write fd_in error.");
+	b_read = read(fd_in, &buff, sizeof(char));
+	while (b_read > 0)
+	{
+		write(fd_out, &buff, 1);
+		b_read = read(fd_in, &buff, sizeof(char));
+	}
+}

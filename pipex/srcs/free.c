9c355f3801836fb74f9514996d5cf43a7f965a23
malshapraboth <malshapraboth@gmail.com>
2025-04-01 15:44:04 +0200

    added clean_up

--- pipex/srcs/free.c
+++ pipex/srcs/free.c
@@ -3,17 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kadferna <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: kadferna <marvin@42.fr>                     +#+  +:+      
+	+#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/01 15:32:38 by kadferna          #+#    #+#             */
-/*   Updated: 2025/04/01 15:32:55 by kadferna         ###   ########.ch       */
+/*   Created: 2025/04/01 15:42:42 by kadferna          #+#    #+#             */
+/*   Updated: 2025/04/01 15:42:49 by kadferna         ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
+static void	free_args(char ***str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str[i] != NULL)
+	{
+		j = 0;
+		while (str[i][j] != NULL)
+		{
+			if (str[i][j])
+				free(str[i][j]);
+			j++;
+		}
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
+static void	free_cmd_path(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != NULL)
+	{
+		if (str[i])
+			free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
 void	clean_up(t_pip *pip)
 {
 	if (!pip)
 		return ;
+	if (pip->cmd_args)
+		free_args(pip->cmd_args);
+	if (pip->cmd_path)
+		free_cmd_path(pip->cmd_path);
+	free(pip);
 }
--- pipex/srcs/main.c
+++ pipex/srcs/main.c
@@ -3,11 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kadferna <marvin@42.fr>                     +#+  +:+      
-	+#+        */
+/*   By: kadferna <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/01 15:32:09 by kadferna          #+#    #+#             */
-/*   Updated: 2025/04/01 15:32:48 by kadferna         ###   ########.ch       */
+/*   Created: 2025/04/01 15:34:36 by kadferna          #+#    #+#             */
+/*   Updated: 2025/04/01 15:34:36 by kadferna         ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,37 +31,7 @@ int	main(int argc, char **argv)
 	// execute_commands(pip, argc, argv);
 	if (pip->here_doc == TRUE)
 		unlink(".here_doc_tmp");
+	close_fd(pip->in_fd[0], pip->out_fd[0]);
 	clean_up(pip);
 	return (0);
 }
-// int	main(int argc, char *argv[])
-// {
-// 	char *line;
-// 	char *limiter;
-// 	int in_fd[2];
-
-// 	in_fd[0] = open(".here_doc_tmp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
-// 	if (in_fd[0] < 0)
-// 		perror("sdf");
-// 	if (argc != 3)
-// 		return (0);
-// 	limiter = ft_strjoin(argv[2], "\n");
-// 	if (!limiter)
-// 		return (0);
-// 	while (1)
-// 	{
-// 		line = get_next_line(0);
-// 		if (!line)
-// 			break ;
-// 		if (ft_strcmp(line, limiter) == 0)
-// 		{
-// 			free(line);
-// 			break ;
-// 		}
-// 		write(in_fd[0], line, ft_strlen(line));
-//         free(line);
-// 	}
-// 	free(limiter);
-// 	close(in_fd[0]);
-// 	return (0);
-// }

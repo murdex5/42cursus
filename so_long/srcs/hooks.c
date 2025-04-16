d276291ff56ee19a09954614ba441ddea8d18b4d
malsha_Vbx <malshapraboth@gmail.com>
2025-04-16 12:03:10 +0200

    can move but problems

--- so_long/srcs/hooks.c
+++ so_long/srcs/hooks.c
@@ -32,12 +32,67 @@ void	on_key_press_exit(t_vars *vars)
 	free_vars(vars);
 }
 
+void	check_bounderies(t_vars *vars)
+{
+	int	x;
+	int	y;
+
+	x = vars->map->player_x;
+	y = vars->map->player_y;
+	if (vars->map->content[y][x] == '1')
+	{
+		while (vars->map->content[y][x] == '1')
+		{
+			if (vars->map->content[y - 1][x] != '1')
+				y--;
+			else if (vars->map->content[y][x - 1] != '1')
+				x--;
+			else if (vars->map->content[y - 1][x - 1] != '1')
+			{
+				y--;
+				x--;
+			}
+			else
+				break ;
+			vars->map->player_x = x;
+			vars->map->player_y = y;
+		}
+	}
+}
+
+void	player_move(int keysym, t_vars *vars)
+{
+	if (keysym == W)
+	{
+		vars->map->player_y -= 1;
+		vars->player->player_state = 1;
+	}
+	if (keysym == A)
+	{
+		vars->map->player_x -= 1;
+		vars->player->player_state = 1;
+	}
+	if (keysym == S)
+	{
+		vars->map->player_y += 1;
+		vars->player->player_state = 1;
+	}
+	if (keysym == D)
+	{
+		vars->map->player_x += 1;
+		vars->player->player_state = 1;
+	}
+}
+
 int	on_keypress(int keysym, t_vars *vars)
 {
+	check_bounderies(vars);
 	if (keysym == ESC)
 	{
 		on_key_press_exit(vars);
 		exit(0);
 	}
+	if (keysym == W || keysym == A || keysym == S || keysym == D)
+		player_move(keysym, vars);
 	return (0);
 }

5eb0f3f99c376538a2087618e977dbe35e3fcf12
malshapraboth <malshapraboth@gmail.com>
2025-04-26 15:42:37 +0200

    stack_b rotate

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -50,4 +50,5 @@ int						pb(t_stack_node **stack_a, t_stack_node **stack_b);
 int						sa(t_stack_node **stack_a);
 int						sb(t_stack_node **stack_b);
 int						ra(t_stack_node **stack_a);
+int						rb(t_stack_node **stack_b);
 #endif
--- push_swap/srcs/rotate_func.c
+++ push_swap/srcs/rotate_func.c
@@ -28,3 +28,20 @@ int	ra(t_stack_node **stack_a)
 	*stack_a = second_node;
 	return (1);
 }
+
+int	rb(t_stack_node **stack_b)
+{
+	t_stack_node *last_node;
+	t_stack_node *second_node;
+
+	if (!stack_b || !*stack_b || !(*stack_b)->next)
+		return (0);
+	last_node = get_last_node(*stack_b);
+	second_node = (*stack_b)->next;
+	(*stack_b)->prev = last_node;
+	(*stack_b)->next = NULL;
+	last_node->next = *stack_b;
+	second_node->prev = NULL;
+	*stack_b = second_node;
+	return (1);
+}

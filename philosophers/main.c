6767d0f68560e99336d727c2ae158d2a417455e2
malshapraboth <kadferna@student.42roma.it>
2025-05-09 13:52:29 +0200

    new commit

--- philosophers/main.c
+++ philosophers/main.c
@@ -10,4 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../philosophers.h"
+#include "philosophers.h"
+
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+        return (1);
+    printf("%s\n", argv[1]);
+    return (0);
+}
--- philosophers/philosophers.h
+++ philosophers/philosophers.h
@@ -11,3 +11,11 @@
 /* ************************************************************************** */
 
 
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#endif

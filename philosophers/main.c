dfa60852d175755aabd37b4f168ca67c4a3b2fce
malshapraboth <malshapraboth@gmail.com>
2025-05-13 14:58:29 +0200

    dont know what i added

--- philosophers/main.c
+++ philosophers/main.c
@@ -12,16 +12,14 @@
 
 #include "philosophers.h"
 
-int	main(int argc, char **argv)
+int     main(int argc, char **argv)
 {
 	pthread_t *restrict thread;
-    const pthread_t *restrict att;
-    void *restrict arg;
-
+	const pthread_t *restrict att;
+	void *restrict arg;
 	if (argc < 2)
 		return (1);
-    if (!pthread_create(thread, att, start_routine(), arg))
-        return (0);
-        
+	if (!pthread_create(thread, att, start_routine(), arg))
+		return (0);
 	return (0);
 }

15293b310c0f4748993d97d134dc9a57153cc68f
malshapraboth <kadferna@student.42roma.it>
2025-05-14 11:09:42 +0200

    added new data types

--- philosophers/philosophers.h
+++ philosophers/philosophers.h
@@ -18,9 +18,34 @@
 # include <time.h>
 # include <unistd.h>
 
-typedef struct s_philosopher
+typedef struct s_philo
 {
-	int	time_to_eat;
-}		t_philosopher;
+	pthread_t		thread;
+	int				id;
+	int				eating;
+	int				meals_eaten;
+	size_t			last_meal;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			start_time;
+	int				num_of_philos;
+	int				num_times_to_eat;
+	int				*dead;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
+}					t_philo;
+
+typedef struct s_program
+{
+	int				dead_flag;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	write_lock;
+	t_philo			*philos;
+}					t_program;
 
 #endif

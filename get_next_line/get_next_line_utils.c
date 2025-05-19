dea028cd4da964a49b31e9ba0217f8c371c8cef7
malshapraboth <kadferna@student.42roma.it>
2025-05-19 09:42:42 +0200

    removed a potential mem alloc error

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -18,7 +18,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 	char	*str;
 
-	str = (char *)malloc(sizeof(*s) * (len + 1));
+	str = (char *)malloc(sizeof(*str) * (len + 1));
 	if (str == 0)
 		return (NULL);
 	i = 0;

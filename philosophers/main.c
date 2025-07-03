df1c42e9f2e7a3cef3a09e8c7afa0849f5e488d1
malshapraboth <malshapraboth@gmail.com>
2025-07-03 21:47:50 +0200

    now everything is working

--- philosophers/main.c
+++ philosophers/main.c
@@ -69,19 +69,27 @@ int	main(int argc, char **argv)
 {
 	int			nums[MAX_PHILOS];
 	t_data		*data;
-	t_philo *philos;
-	// int			i;
-	// pthread_t	monitor_thread;
+	t_philo		*philos;
+	int			i;
+	pthread_t	monitor_thread;
 
-	//i = -1;
-	philos = NULL;
+	i = -1;
 	if (!checks(nums, argc, argv))
 		return (1);
 	data = init(nums, &philos);
 	if (!data)
 		return (1);
+	while (++i < data->num_philos)
+	{
+		if (pthread_create(&philos[i].thread, NULL, routine, &philos[i]) != 0)
+			return (1);
+	}
+	pthread_create(&monitor_thread, NULL, monitor, philos);
+	i = -1;
+	while (++i < data->num_philos)
+		pthread_join(philos[i].thread, NULL);
+	pthread_join(monitor_thread, NULL);
 	if (data)
 		free_data_struct(data, philos);
-	// free(philos);
 	return (0);
 }

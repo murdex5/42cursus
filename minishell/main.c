2ba539d9452b727f62b759445d809767a0823c93
malshapraboth <malshapraboth@gmail.com>
2025-07-14 14:54:16 +0200

    some improvements nothing much

--- minishell/main.c
+++ minishell/main.c
@@ -12,12 +12,33 @@
 
 #include "minishell.h"
 
+volatile sig_atomic_t	g_signal_recieved = 0;
+
+void	signal_handler(int sig)
+{
+	g_signal_recieved = sig;
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
 int	main(void)
 {
-	char	*line;
+	char				*line;
+	struct sigaction	sa;
 
+	sa.sa_handler = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		exit(1);
+	}
 	while (1)
 	{
+		if (g_signal_recieved)
+			g_signal_recieved = 0;
 		line = readline("minishell> ");
 		if (line == NULL)
 			return (ft_exit(line), 0);
